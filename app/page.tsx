e83fbeba52b7ace37f7f9f8c2182cf8e1ec50c51
alexcosteach <alexcosteach@gmail.com>
2026-05-16 17:49:32 +0300

    optimize mobile design

--- app/page.tsx
+++ app/page.tsx
@@ -80,6 +80,8 @@ export default function Home() {
     "/portfolio/mao-equipment-1.jpg",
     "/portfolio/mao-equipment-2.jpg",
     "/portfolio/mao-branding-1.jpg",
+    "/portfolio/mao-branding-2.jpg",
+  
   ];
 
   return (
